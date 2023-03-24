55cb6ce55a27533cd79058472d38e5122fd66d79
Josh Bennett <joshbennett793@yahoo.com>
2023-03-23 23:44:46 -0500

    Style main weather el and add bg and icon assets
    Hardcoded icon svgs into html. Changed font to
    Comfortaa. Added background images to assets folder
    and will be adding background change functionality.
    Also plan to implement drop down menu for search
    and background change. Search bar is currently at
    top of page as of this commit, as is date and time.

--- src/index.js
+++ src/index.js
@@ -8,15 +8,16 @@ import {
   getLocationFromUserInput,
   writeLocalDateTime,
 } from './components/header/header';
+import './components/weather/main-weather-ui.css';
 
 async function writeCurrentTemps(location, unit) {
   try {
     const currentTemps = await extractCurrentTemps(location, unit);
     const currentTemp = document.getElementById('current-temp');
-    const feelsLike = document.getElementById('feels-like');
+    const feelsLike = document.getElementById('feels-like-temp');
 
-    currentTemp.textContent = `Currently: ${currentTemps.temp}°`;
-    feelsLike.textContent = `Feels Like: ${currentTemps.feels_like}°`;
+    currentTemp.textContent = `${currentTemps.temp}°`;
+    feelsLike.textContent = `${currentTemps.feels_like}°`;
   } catch (err) {
     console.error('There was an error writing temps to the DOM: ', err);
   }
@@ -25,9 +26,9 @@ async function writeCurrentTemps(location, unit) {
 async function writeHumidity(location, unit) {
   try {
     const currentHumidity = await extractHumidity(location, unit);
-    const humiditySpan = document.getElementById('humidity');
+    const humidity = document.getElementById('humidity-percentage');
 
-    humiditySpan.textContent = `Humidity: ${currentHumidity}%`;
+    humidity.textContent = `${currentHumidity}%`;
   } catch (err) {
     console.error('There was an error writing humidity to the DOM: ', err);
   }
@@ -36,9 +37,9 @@ async function writeHumidity(location, unit) {
 async function writePrecipitation(location, unit) {
   try {
     const currentPrecipitation = await extractPrecipitation(location, unit);
-    const precipitationSpan = document.getElementById('precipitation');
+    const precipitation = document.getElementById('precipitation-percentage');
 
-    precipitationSpan.textContent = `Precipitation: ${currentPrecipitation}%`;
+    precipitation.textContent = `${currentPrecipitation}%`;
   } catch (err) {
     console.error('There was an error writing precipitation to the DOM: ', err);
   }
@@ -47,12 +48,12 @@ async function writePrecipitation(location, unit) {
 async function writeWindSpeed(location, unit) {
   try {
     const currentWindSpeed = await extractWindSpeed(location, unit);
-    const windSpeedSpan = document.getElementById('wind-speed');
+    const windSpeed = document.getElementById('wind-speed-percentage');
 
-    windSpeedSpan.textContent =
+    windSpeed.textContent =
       unit === 'imperial'
-        ? `Wind Speed: ${currentWindSpeed} MPH`
-        : `Wind Speed: ${currentWindSpeed} KM/H`;
+        ? `${currentWindSpeed} MPH`
+        : `${currentWindSpeed} KM/H`;
   } catch (err) {
     console.error('There was an error writing windspeed to the DOM: ', err);
   }
