f1a770798bef9e02324d1eb322803d22551c5195
Josh Bennett <joshbennett793@yahoo.com>
2023-03-29 10:49:38 -0500

    Add functionality to toggle background image and menu color palette
    This commit adds a new feature to the menu.js file that allows the user
    to toggle the background image and menu color palette by clicking on the
    background span in the menu. When the user clicks on the background span,
    the background image changes, and the menu color palette updates to match
    the new background image.
    
    Additionally, this commit removes the date-fns dependency from the project,
    as the decision was made to remove the date and time functionality from
    the UI, resulting in a cleaner interface.
    
    Note that this commit is still a work in progress, as the API calls are
    not yet pulling data due to the missing search bar, which will be added
    in the next update along with the API calls.

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,1 +1,29 @@
 import './menu.css';
+
+const backgroundBtn = document.querySelector('.background-selector');
+const contentContainer = document.getElementById('content');
+
+const getPngFileNameFromBackground = () => {
+  const contentContainerBackgroundImg = window
+    .getComputedStyle(contentContainer)
+    .getPropertyValue('background-image');
+  const pngFileName = contentContainerBackgroundImg.match(/\b(\w+)\.png\b/)[1];
+  return pngFileName;
+};
+
+const toggleMenuBackgroundColor = () => {
+  const menu = document.getElementById('menu');
+  menu.className = getPngFileNameFromBackground();
+};
+
+const changeBackground = () => {
+  let pngFileName = getPngFileNameFromBackground();
+  const fileName = pngFileName === 'river' ? 'city' : 'river';
+  contentContainer.style.backgroundImage = `url(../assets/backgrounds/${fileName}.png)`;
+  pngFileName = fileName;
+};
+
+backgroundBtn.onclick = () => {
+  changeBackground();
+  toggleMenuBackgroundColor();
+};
